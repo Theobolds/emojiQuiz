338a70476d6703befdea79e8ce45c05ed8f8130f
Alex Theobold <theoboldalex@gmail.com>
2020-03-24 17:43:18 +0000

    Initial Commit

---
+++ emojiQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emojiQuiz
+//
+//  Created by alex theobold on 24/03/2020.
+//  Copyright © 2020 alex theobold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
