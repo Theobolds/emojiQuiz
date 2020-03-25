b6006fb94deae9686bc9c4eaa65d9ecfe175d943
Alex Theobold <theoboldalex@gmail.com>
2020-03-25 10:58:38 +0000

    Setup for resultViewController completed. View controllers grouped to folder

--- emojiQuiz/Controllers/QuizViewController.swift
+++ emojiQuiz/Controllers/QuizViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QuizViewController: UIViewController {
     
     var quizBrain = QuizBrain()
     let emoji = UILabel()
---
+++ emojiQuiz/Controllers/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  emojiQuiz
+//
+//  Created by alex theobold on 25/03/2020.
+//  Copyright © 2020 alex theobold. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
